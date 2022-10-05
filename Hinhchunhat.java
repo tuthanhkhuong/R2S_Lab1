9bd728948c4e41b353355bb20473517c6225b09b
Williump <tuthanhkhuong@gmail.com>
2022-10-05 19:48:58 +0700

    Lab1_Bai1

---
+++ Hinhchunhat.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Hinhchunhat {
+    public static void main (String [] args){
+        //khai bao bien
+        double length, width;
+        double area, cir;
+
+        //nhap du lieu
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap chieu dai: ");
+        length=sc.nextDouble();
+        System.out.print("Nhap chieu rong: ");
+        width=sc.nextDouble();
+
+        //tinh chu vi, dien tich
+        area=length*width;
+        cir=(length*width)/2;
+        System.out.println("Area = " +area);
+        System.out.println("Cir = " +cir);
+
+
+
+    }
+}
