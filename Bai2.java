9599a24967dd3735f387e29a7732671f7f78b1c9
Williump <tuthanhkhuong@gmail.com>
2022-10-05 20:01:06 +0700

    commit

--- Bai2.java
+++ Bai2.java
@@ -2,6 +2,35 @@ import java.util.Scanner;
 
 public class Bai2 {
     public static void main(String[] args) {
-        
+        int pack, distance;
+        int shift,feedback;
+        int call, electric;
+        int other;
+        double salary;
+
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Nhap package: ");
+        pack = sc.nextInt();
+
+        System.out.print("Nhap distance: ");
+        distance = sc.nextInt();
+
+        System.out.print("Nhap shift: ");
+        shift = sc.nextInt();
+
+        System.out.print("Nhap feedback: ");
+        feedback = sc.nextInt();
+
+        System.out.print("Nhap call: ");
+        call = sc.nextInt();
+
+        System.out.print("Nhap electric: ");
+        electric = sc.nextInt();
+
+        System.out.print("Nhap other: ");
+        other = sc.nextInt();
+
+        salary = pack * 50 + distance * 75 + shift * 50 * 0.1 + feedback + call + electric + other;
+        System.out.print("Salary: " +salary);
     }
 }
