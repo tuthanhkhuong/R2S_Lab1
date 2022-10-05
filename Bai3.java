67aadc442e9bee7110ab5844384b3ec6bf8f6218
Williump <tuthanhkhuong@gmail.com>
2022-10-05 20:38:18 +0700

    commit

--- Bai3.java
+++ Bai3.java
@@ -2,24 +2,21 @@ import java.util.Scanner;
 
 public class Bai3 {
 
-    public static void TongHieuTichThuong(int x, int y){
-
+    public static void main(String[] args){
+        int x;
+        int y;
         Scanner sc= new Scanner(System.in);
 
-        System.out.print("Nhap x: ");
+        System.out.print("Nhap vao x: ");
         x = sc.nextInt();
 
-        System.out.print("Nhap y: ");
+        System.out.print("Nhap vao y: ");
         y = sc.nextInt();
 
-        int tong= x+y;
-        int hieu= x-y;
-        int tich=x*y;
-        int thuong= x/y;
-
-        System.out.print("tong: "+ tong);
-        System.out.print("hieu: "+ hieu);
-        System.out.print("tich: "+ tich);
-        System.out.print("thuong: "+ thuong);
+        System.out.println("tong: "+ (x+y));
+        System.out.println("hieu: "+ (x-y));
+        System.out.println("tich: "+ (x*y));
+        System.out.println("thuong: " + (float)x/y);
+        System.out.println("phandu: "+ (x%y));
     }
 }
