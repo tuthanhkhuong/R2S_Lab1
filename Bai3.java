6953e46329dbebdf429b19a89ff68fe825790c7a
Williump <tuthanhkhuong@gmail.com>
2022-10-05 20:59:49 +0700

    commit

--- Bai3.java
+++ Bai3.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Bai3 {
 
-    public static void main(String[] args){
+    // Bai 3.1 Nhap 2 so x va y tinh tong hieu tich thuong
+    /*public static void main(String[] args){
         int x;
         int y;
         Scanner sc= new Scanner(System.in);
@@ -18,5 +19,36 @@ public class Bai3 {
         System.out.println("tich: "+ (x*y));
         System.out.println("thuong: " + (float)x/y);
         System.out.println("phandu: "+ (x%y));
+    }*/
+
+
+    // Bai 3.2 Chuyen do C sang do F
+    /*public static void main(String[] args){
+        double c;
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Nhap vao do C: ");
+        c = sc.nextDouble();
+
+        double f=  (double) (9*c/5) + 32 ;
+        System.out.println(c+ "Do C = "+ f+ "Do F" );
+
+    }*/
+
+
+    // Bai 3.3 Tinh tong va trung binh cong
+    public static void main(String[] args){
+        double x,y,z;
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Nhap vao diem Toan: ");
+        x = sc.nextDouble();
+        System.out.print("Nhap vao diem Ly: ");
+        y = sc.nextDouble();
+        System.out.print("Nhap vao diem Hoa: ");
+        z = sc.nextDouble();
+
+        double tbc = (x+y+z)/ 3;
+        System.out.println("Tong diem 3 mon = " + (x+y+z));
+        System.out.println("Trung binh cong diem 3 mon = " +tbc);
     }
 }
