8ece8a824f4713f7be119175493a0a07a5572a3d
Williump <tuthanhkhuong@gmail.com>
2022-10-13 18:28:23 +0700

    Liet ke le

---
+++ Lab4/Bai1.java
@@ -0,0 +1,14 @@
+package Lab4;
+
+
+public class Bai1 {
+    public static void main (String [] args){
+        int arr[] = {10, 25, -4, 32, 63, 81, 19, -24, 13, 18, 45, 12, 72, 42, -6};
+
+        for(int i = 0; i <= arr.length; i++) {
+            if (arr[i] % 2 == 1){
+                System.out.println("So le trong mang la: " + arr[i] + " tai vi tri " + i);
+            }
+        }
+    }
+}
