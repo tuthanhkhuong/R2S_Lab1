bc7294dc6a5761489e117871938c7c197102a599
Williump <tuthanhkhuong@gmail.com>
2022-10-13 18:49:43 +0700

    Liet ke chia het cho 2 va 6

---
+++ Lab4/Bai1/YC2.java
@@ -0,0 +1,16 @@
+package Lab4.Bai1;
+
+public class YC2 {
+    public static void main (String [] args){
+        int arr[] = {10, 25, -4, 32, 63, 81, 19, -24, 13, 18, 45, 12, 72, 42, -6};
+
+        int cout=0;
+
+        for(int i = 0; i < arr.length; i++) {
+            if (arr[i]%2==0 && arr[i]%6==0){
+                cout++;
+            }
+        }
+        System.out.println("So phan tu la: " + cout);
+    }
+}
