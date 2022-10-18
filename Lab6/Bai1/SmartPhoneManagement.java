c945614d6fcac1692eb4a087a807ef6e7867540a
Williump <tuthanhkhuong@gmail.com>
2022-10-18 19:53:50 +0700

    Nhap dien thoai

--- Lab6/Bai1/SmartPhoneManagement.java
+++ Lab6/Bai1/SmartPhoneManagement.java
@@ -4,11 +4,8 @@ public class SmartPhoneManagement {
     public static void main (String[] args){
         Properties s1 = new Properties();
         s1.inputPhone();
-        s1.showInfo();
 
-        Properties s2 = new Properties();
-        s2.inputPhone();
-        s2.showInfo();
+        Properties s2 = new Properties("ip13 Pro X","Apple",156,1200);
 
         System.out.println(s1.comparePhone(s2));
 
